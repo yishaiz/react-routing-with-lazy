3a32ff10aafd49f82e385435866d675eff9abe5d
yishai <yishai5555@gmail.com>
2019-12-07 22:29:46 +0200

    b1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
+// import Posts from "./containers/Posts";
 
-import Posts from './containers/Posts';
-import User from './containers/User';
-import Welcome from './containers/Welcome';
-
+const Posts = React.lazy(() => import("./containers/Posts"));
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <React.Fragment>
+        {/* <React.Fragment> */}
+        <>
           <nav>
             <NavLink to="/user">User Page</NavLink> |&nbsp;
             <NavLink to="/posts">Posts Page</NavLink>
           </nav>
           <Route path="/" component={Welcome} exact />
           <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
-        </React.Fragment>
+          {/* <Route path="/posts" component={Posts} /> */}
+        <Route
+          path="/posts"
+          component={() => (
+            <Suspense fallback={<div>Loading ...</div>}> 
+              <Posts />
+            </Suspense>
+          )}
+        />
+        </>
+        {/* </React.Fragment> */}
       </BrowserRouter>
     );
   }
